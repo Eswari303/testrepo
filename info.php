667f2b056537d1df55ec96de6e0e582340d092ec
Eswari303 <eswari.hima@gmail.com>
2017-06-20 13:04:07 +0530

    Add files via upload

---
+++ info.php
@@ -0,0 +1,27 @@
+ <?php
+echo " welcome";
+$servername = "localhost";
+$username = "root";
+$password = "Pass@1234";
+$dbname = "eswaridb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, email FROM authors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
