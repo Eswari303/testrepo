7253f5c6cfa0e174ef2da5a6c09525db7a4677ba
Eswari303 <eswari.hima@gmail.com>
2017-08-08 12:48:22 +0530

    Update info.php

--- info.php
+++ info.php
@@ -14,11 +14,10 @@ if ($conn->connect_error) {
 
 $sql = "SELECT id, name, email FROM authors";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
+        echo "Hello ". $row["name"]. " your Email is: " . $row["email"]. "<br>";
     }
 } else {
     echo "0 results";
